cc9be96d5d682d8a0c43941b7bc095e8388f1c54
Joshua Landman <joshua.s.landman@gmail.com>
2026-06-20 05:30:55 -0700

    completed migration

--- scripts/cloud-build-grant.sh
+++ scripts/cloud-build-grant.sh
@@ -1,7 +1,8 @@
 # Find your project number (it is 901961412597 based on your WIF string)
 PROJECT_NUMBER="901961412597"
 PROJECT_ID="rollingblackoutapp"
-CB_SA="${PROJECT_NUMBER}@://gserviceaccount.com"
+CB_SA="band-portal-deploy@rollingblackoutapp.iam.gserviceaccount.com";
+#{PROJECT_NUMBER}@://gserviceaccount.com"
 
 # Allow Cloud Build to manage Cloud Run services
 gcloud projects add-iam-policy-binding $PROJECT_ID \
