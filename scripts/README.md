95fb7ef441473737fc4f194eda3d71c3765c4871
Joshua Landman <joshua.s.landman@gmail.com>
2026-07-13 11:27:33 -0700

    fixd deploy

--- scripts/README.md
+++
@@ -1,6 +0,0 @@
-# initial dev scripts 
-
-# replace with terraform 
-
-Most the terraform has moved new location
-
--- scripts/script.zsh
+++
@@ -1,8 +0,0 @@
-perl -i.bak.183 -pe ' s/captainjspace/${git_acct}/g ' 03.tags.domains.zsh
-perl -i.bak.184 -pe ' s/github/${git_oidc}/g ' 03.tags.domains.zsh
-perl -i.bak.187 -pe ' s/rollingblackoutband.com/${google_org}/g ' 03.tags.domains.zsh
-perl -i.bak.192 -pe ' s/us-central1/${region}/g ' 03.tags.domains.zsh
-perl -i.bak.196 -pe ' s/global/${wip_location}/g ' 03.tags.domains.zsh
-perl -i.bak.197 -pe ' s/githubpool/${wip_pool}/g ' 03.tags.domains.zsh
-perl -i.bak.199 -pe ' s/rollingblackoutband.com/${primary_domain}/g ' 03.tags.domains.zsh
-perl -i.bak.200 -pe ' s/rollingblackout.band/${secondary_domain}/g ' 03.tags.domains.zsh
